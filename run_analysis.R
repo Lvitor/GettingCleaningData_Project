99d4b447be29f3e844bbf197b42f2fce46027864
LEONARDO VITOR ARAUJO VITOR ARAUJO CAVALCANTI <leonardo.cavalcantit@gmail.com>
2014-05-24 13:15:17 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,10 +2,8 @@ run_analysis <- function(){
         
         # Read file with labels and list files structure for meger.
         
-        activity_labes = read.table("./UCI HAR Dataset/activity_labels.txt",head=FALSE,sep=" ")
-        features = read.csv("./UCI HAR Dataset/features.txt",head=FALSE,sep=" ")
         
-        activity_labes_transp = t(activity_labes[,2])
+        features = read.csv("./UCI HAR Dataset/features.txt",head=FALSE,sep=" ")
         features_transp = t(features[,2])
         
         List_File_test = list.files("./UCI HAR Dataset/test",recursive=TRUE)
@@ -29,21 +27,27 @@ run_analysis <- function(){
                                 sep="",head=FALSE,dec=".")
                         
                         datcombine = rbind(dat_train,dat_test)
-                        #datcombine = sapply(datcombine,as.numeric)
+                        
                         
                         if (basename(List_File_test_temp[i]) == "X.txt"){
                                 
                                 colnames(datcombine) = features_transp
-                                write.csv(datcombine,file=basename(List_File_test_temp[i]),row.names = FALSE)
+                                write.table(datcombine,file=basename(List_File_test_temp[i]),row.names = FALSE)
                                 x = datcombine
                                 
                         }else
                         
                         if (basename(List_File_test_temp[i]) == "y.txt"){
-                                
-                                datcombine = merge(datcombine,activity_labes,by="V1",sort=FALSE)
-                                names(datcombine)[1] = "activity_labes"
-                                names(datcombine)[2] = "activity"
+                              
+                                datcombine["name"] = 0
+                                datcombine$name[datcombine$V1 == 1] = "WALKING"
+                                datcombine$name[datcombine$V1 == 2] = "WALKING_UPSTAIRS"
+                                datcombine$name[datcombine$V1 == 3] = "WALKING_DOWNSTAIRS"
+                                datcombine$name[datcombine$V1 == 4] = "SITTING"
+                                datcombine$name[datcombine$V1 == 5] = "STANDING"
+                                datcombine$name[datcombine$V1 == 6] = "LAYING"
+                                names(datcombine)[1] = "activity"
+                                names(datcombine)[2] = "activity_labes"
                                 write.csv(datcombine,file=basename(List_File_test_temp[i]),row.names = FALSE)
                                 y = datcombine
                                 
@@ -58,32 +62,37 @@ run_analysis <- function(){
                                 write.csv(datcombine,file=basename(List_File_test_temp[i]),row.names = FALSE)
                             
                          
-                        }                       
+                        }                          
                        
                 }
              }
-        
-            
              
         }
         
         #Create a tiny data with only mean and std
         
+        
         split_mean = grep("mean", colnames(x))
         split_std = grep("std", colnames(x))
         tiny_x = x[,sort(c(split_mean,split_std))]      
-        merge_subject_activ = cbind(subject,y)      
-        tiny_data = cbind(merge_subject_activ,tiny_x)
-        names(tiny_data)[1] = "subject"
-        write.table(tiny_data,file="./tiny_data.txt",row.names = FALSE)
+        tiny_data = cbind(subject,y,tiny_x)
+        write.csv2(tiny_data,file="./tiny_data.txt",sep=";",row.names = FALSE)
         
         
         #Create a tiny data with mean for each variable for each subject
         
         
-        merge = cbind(subject,y,x,deparse.level=1)
-        names(merge)[1] = "subject"        
-        subject_mean  = aggregate(merge[,4:length(merge)], by=list(merge$subject,merge$activity), FUN='mean')
-        write.table(subject_mean,"./tiny_dat_by_subj.txt",row.names = FALSE)
+        merge = cbind(subject,y,x)
+        subject_mean  = aggregate(merge[,3:length(merge)], by=list(merge$subject,merge$activity), FUN='mean')
+        names(subject_mean)[1] = "subject"
+        names(subject_mean)[2] = "activity"
+        subject_mean$activity_labes[subject_mean$activity == 1] = "WALKING"
+        subject_mean$activity_labes[subject_mean$activity == 2] = "WALKING_UPSTAIRS"
+        subject_mean$activity_labes[subject_mean$activity == 3] = "WALKING_DOWNSTAIRS"
+        subject_mean$activity_labes[subject_mean$activity == 4] = "SITTING"
+        subject_mean$activity_labes[subject_mean$activity == 5] = "STANDING"
+        subject_mean$activity_labes[subject_mean$activity == 6] = "LAYING"
         
+        write.csv2(subject_mean,"./tiny_dat_by_subj.txt",sep=";",row.names = FALSE)
+  
 }
